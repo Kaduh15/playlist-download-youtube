82c848ae545f73eb0f80d04ea9f546ac4376ec81
Kadu <kadu.silva2014@gmai.com>
2024-03-21 20:13:40 -0300

    Feat: Cria rota para download de um video

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,50 @@
 import Fastify from 'fastify'
+import { ZodError, z } from 'zod'
+import ytdl from 'ytdl-core'
 
 const fastify = Fastify({
   logger: true,
 })
 
-// Declare a route
-fastify.get('/', function (request, reply) {
-  reply.send({ hello: 'world' })
+fastify.setErrorHandler((error, request, reply) => {
+  if (error instanceof ZodError) {
+    reply.status(400).send({
+      ok: false,
+      error: error.errors.map((err) => ({
+        message: err.message,
+      })),
+    })
+  }
+
+  reply.status(500).send({ ok: false })
+})
+
+fastify.get('/', async function (request, reply) {
+  const { url: videoUrl } = z
+    .object({
+      url: z
+        .string({
+          required_error: 'URL do vídeo do YouTube não fornecida',
+        })
+        .url(),
+    })
+    .parse(request.query)
+  if (!ytdl.validateURL(videoUrl)) {
+    reply.code(400).send({ error: 'URL do vídeo do YouTube inválida' })
+    return
+  }
+
+  const info = await ytdl.getInfo(videoUrl)
+
+  const format = ytdl.chooseFormat(info.formats, {
+    quality: 'highest',
+    filter: 'audioonly',
+  })
+
+  // Construir o link de download
+  const downloadLink = format.url
+
+  reply.send({ downloadLink })
 })
 
 // Run the server!
